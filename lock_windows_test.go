89f6d9d39b6681e2fae734f33b403a5cf68c1d78
Stephen Fox <stephen.j.fox.jr@gmail.com>
2019-03-06 23:12:40 -0500

    Initial Windows unit tests.

---
+++ lock_windows_test.go
@@ -0,0 +1,98 @@
+// +build windows
+
+package lock
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestDefaultAcquirer_Acquire(t *testing.T) {
+	env := setupLockFileTestEnv(t)
+	lockName := randomAlphaString(10)
+
+	l, err := NewAcquirer().
+		SetLocation(lockName).
+		Acquire()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	defer func() {
+		err := l.Release()
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	}()
+
+	o := testHarnessOptions{
+		lockLocation: lockName,
+	}
+
+	_, err = prepareTestHarness(env, o, t).CombinedOutput()
+	if err == nil {
+		t.Fatal("expected test harness lock acquire to fail, but it did not")
+	}
+}
+
+func TestDefaultAcquirer_Acquire_CustomTimeout(t *testing.T) {
+	env := setupLockFileTestEnv(t)
+	lockName := randomAlphaString(10)
+	testHarness := newProcessAcquiresLockAndIdles(env, lockName, t)
+	defer func() {
+		err := testHarness.Process.Kill()
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	}()
+
+	acquireTimeout := 5 * time.Second
+	start := time.Now()
+	l, err := NewAcquirer().
+		SetLocation(lockName).
+		SetAcquireTimeout(acquireTimeout).
+		Acquire()
+	if err == nil {
+		l.Release()
+		t.Fatal("expected acquire attempt to fail")
+	}
+
+	duration := time.Since(start)
+	if duration < acquireTimeout {
+		t.Fatalf("timeout only lasted %s when it should have taken at least %s",
+			duration.String(), acquireTimeout.String())
+	}
+}
+
+func TestDefaultAcquirer_Acquire_AlreadyAcquired(t *testing.T) {
+	env := setupLockFileTestEnv(t)
+	lockName := randomAlphaString(10)
+	testHarness := newProcessAcquiresLockAndIdles(env, lockName, t)
+	defer func() {
+		err := testHarness.Process.Kill()
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	}()
+
+	l, err := NewAcquirer().
+		SetLocation(lockName).
+		Acquire()
+	if err == nil {
+		l.Release()
+		t.Fatal("expected acquire attempt to fail")
+	}
+}
+
+// randomAlphaString by "icza":
+// https://stackoverflow.com/a/31832326
+func randomAlphaString(n int) string {
+	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Int63() % int64(len(letterBytes))]
+	}
+
+	return string(b)
+}
