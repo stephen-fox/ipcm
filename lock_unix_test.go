dad9cce84280d1ea9c997504e2b45d7d87ded4c8
Stephen Fox <stephen.j.fox.jr@gmail.com>
2019-03-18 22:12:33 -0400

    Added more advanced IPC / multi-routine tests.

--- lock_unix_test.go
+++ lock_unix_test.go
@@ -3,8 +3,10 @@
 package lock
 
 import (
-	"log"
+	"io/ioutil"
 	"path"
+	"strconv"
+	"sync"
 	"testing"
 	"time"
 )
@@ -97,5 +99,128 @@ func TestNewMutex_TryLock(t *testing.T) {
 	err = m.TryLock()
 	if err == nil {
 		t.Fatal("expected acquire attempt to fail")
+	}
+
+	testHarness.Process.Kill()
+	testHarness.Wait()
+
+	err = m.TryLock()
+	if err != nil {
+		t.Fatalf("lock attempt failed after killing holder - %s", err.Error())
+	}
+	m.Unlock()
+}
+
+func TestNewMutex_MultipleRoutines(t *testing.T) {
+	env := setupLockFileTestEnv(t)
+	lockFilePath := path.Join(env.dataDirPath, lockFileName)
+	m, err := NewMutex(lockFilePath)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	const exp = 100
+	result := 0
+	wg := &sync.WaitGroup{}
+	wg.Add(exp)
+
+	for i := 0; i < exp; i++ {
+		go func() {
+			m.Lock()
+			result++
+			m.Unlock()
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	if result != exp {
+		t.Fatal("got", result, "- expected", exp)
+	}
+}
+
+func TestNewMutex_MultipleRoutinesIpc(t *testing.T) {
+	env := setupLockFileTestEnv(t)
+
+	ipcFilePath := path.Join(env.dataDirPath, "whatever.txt")
+	err := ioutil.WriteFile(ipcFilePath, []byte{'0'}, 0600)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	const expected = 100
+	const half = expected / 2
+
+	lockFilePath := path.Join(env.dataDirPath, lockFileName)
+	options := testHarnessOptions{
+		resource:    lockFilePath,
+		ipcFilePath: ipcFilePath,
+		ipcValue:    half,
+	}
+
+	testHarness := prepareTestHarness(env, options, t)
+
+	m, err := NewMutex(lockFilePath)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	err = testHarness.Start()
+	if err != nil {
+		t.Fatalf("failed to start test hanress - %s", err.Error())
+	}
+	defer func() {
+		testHarness.Process.Kill()
+		testHarness.Wait()
+	}()
+
+	wg := &sync.WaitGroup{}
+	wg.Add(half)
+
+	for i := 0; i < half; i++ {
+		go func() {
+			m.Lock()
+			defer m.Unlock()
+
+			raw, err := ioutil.ReadFile(ipcFilePath)
+			if err != nil {
+				t.Fatalf("failed to read IPC test file - %s", err.Error())
+			}
+
+			v, err := strconv.Atoi(string(raw))
+			if err != nil {
+				t.Fatalf("failed to read an integer from IPC test file - %s", err.Error())
+			}
+
+			v++
+			err = ioutil.WriteFile(ipcFilePath, []byte(strconv.Itoa(v)), 0600)
+			if err != nil {
+				t.Fatalf("failed to write to IPC test file - %s", err.Error())
+			}
+
+			wg.Done()
+		}()
+	}
+
+	err = testHarness.Wait()
+	if err != nil {
+		t.Fatalf("failed to wait for test harness - %s", err.Error())
+	}
+
+	wg.Wait()
+
+	raw, err := ioutil.ReadFile(ipcFilePath)
+	if err != nil {
+		t.Fatalf("failed to read final value from IPC test file - %s", err.Error())
+	}
+
+	final, err := strconv.Atoi(string(raw))
+	if err != nil {
+		t.Fatalf("failed to convert final value - %s", err.Error())
+	}
+
+	if final != expected {
+		t.Fatalf("final value in IPC test file should be %d - got %d", expected, final)
 	}
 }
